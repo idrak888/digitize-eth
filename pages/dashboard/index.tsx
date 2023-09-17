a0a04d85703a1d348694fe8ae2c339544ab2011b
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-09-17 07:53:00 -0400

    feat verified

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,26 +1,24 @@
-import React, {  } from "react";
-import {
-  useContract,
-  useDirectListings,
-} from "@thirdweb-dev/react";
-import MintButton from "@/components/MintButton";
+import React from 'react';
+import { useContract, useDirectListings } from '@thirdweb-dev/react';
+import MintButton from '@/components/MintButton';
 import {
   MUMBAI_DIGITIZE_ETH_ADDRESS,
   MUMBAI_MARKETPLACE_ADDRESS,
-} from "@/constant/addresses";
-import { useAddress } from "@thirdweb-dev/react";
-import Head from "next/head";
-import Navbar from "@/components/Navbar";
-import { useLocalStorage } from "usehooks-ts";
-import { User } from "@prisma/client";
-import { USER_LOCAL_STORAGE_KEY } from "@/config";
-import Loading from "@/components/Loading";
-import { PersonalNFTCard } from "@/components/PersonalNFT";
+} from '@/constant/addresses';
+import { useAddress } from '@thirdweb-dev/react';
+import Head from 'next/head';
+import Navbar from '@/components/Navbar';
+import { useLocalStorage } from 'usehooks-ts';
+import { User } from '@prisma/client';
+import { USER_LOCAL_STORAGE_KEY } from '@/config';
+import Loading from '@/components/Loading';
+import { PersonalNFTCard } from '@/components/PersonalNFT';
+import { MdVerified } from 'react-icons/md';
 
 export default function index() {
   const { contract: marketplace } = useContract(
     MUMBAI_MARKETPLACE_ADDRESS,
-    "marketplace-v3"
+    'marketplace-v3'
   );
 
   const { data: directListings } = useDirectListings(marketplace, {
@@ -28,10 +26,7 @@ export default function index() {
   });
 
   const address = useAddress();
-  const [user, setUser] = useLocalStorage<User | null>(
-    USER_LOCAL_STORAGE_KEY,
-    null
-  );
+  const [user, setUser] = useLocalStorage<User | null>(USER_LOCAL_STORAGE_KEY, null);
 
   return (
     <div>
@@ -44,51 +39,56 @@ export default function index() {
       <Navbar />
 
       <div
-        className="container"
+        className='container'
         style={{
           marginTop: 100,
         }}
       >
-        <h1 style={{ fontSize: 30, fontWeight: "bold", marginLeft: 20 }}>
-          Welcome Back,{" "}
+        <h1 style={{ fontSize: 30, fontWeight: 'bold', marginLeft: 20 }}>
+          Welcome Back,{' '}
           {address
             ? `${address.substring(0, 6)}...${address.substring(
                 address.length - 5,
                 address.length - 1
               )}`
-            : "Not signed in"}
+            : 'Not signed in'}
           !
         </h1>
-        <div
-          className="row"
-          style={{ borderBottom: "1px solid grey", padding: 20 }}
-        >
-          <div className="col-sm-2">
+        <div className='row' style={{ borderBottom: '1px solid grey', padding: 20 }}>
+          <div className='col-sm-2'>
             <img
-              src="dp.png"
-              alt="Profile Photo"
+              src='dp.png'
+              alt='Profile Photo'
               style={{
-                width: "100%", // Adjust the size as needed
-                borderRadius: "50%", // Makes the image circular
-                marginRight: "20px", // Adds some space between the photo and the text
-                paddingBottom: "20px",
+                width: '100%', // Adjust the size as needed
+                borderRadius: '50%', // Makes the image circular
+                marginRight: '20px', // Adds some space between the photo and the text
+                paddingBottom: '20px',
               }}
             />
           </div>
 
-          <div className="col-sm-10">
+          <div className='col-sm-10'>
             {user ? (
               <div suppressHydrationWarning>
                 <h3 suppressHydrationWarning>{user.name}</h3>
                 <p suppressHydrationWarning>{user.email}</p>
                 {address ? (
                   <div>
-                    {/* {address.substring(0, 6).concat('...')}
-                      {address.substring(address.length - 5, address.length - 1)} */}
+                    <div>
+                      <MdVerified />
+                      <>&nbsp;Profile verified on, </>
+                      <a style={{
+                        textDecoration: 'underline',
+                      }}
+                      href='https://sepolia.easscan.org/attestation/view/0x18d53b53e90d7cb9b37b2f8ae0d757d1b298baae3b5767008e2985a5894d6d2c'>
+                        Ethereum Attestation Service, view here.
+                      </a>
+                    </div>
                     {address}
                   </div>
                 ) : (
-                  "Not signed in"
+                  'Not signed in'
                 )}
               </div>
             ) : (
@@ -102,27 +102,27 @@ export default function index() {
         </div>
 
         <div
-          className="row"
+          className='row'
           style={{
             padding: 20,
           }}
         >
           <div
             style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
             }}
           >
             <h4>Your Items</h4>
             <MintButton />
           </div>
-          <div className="row">
+          <div className='row'>
             {!!directListings ? (
               directListings
                 ?.filter((listing) => listing.creatorAddress === address)
                 .map((listing, index) => (
-                  <div className="card" key={index}>
+                  <div className='card' key={index}>
                     <PersonalNFTCard
                       contractAddress={listing.assetContractAddress}
                       tokenId={listing.tokenId}
